cf4a800593061e6bbc938f7b5dc61628128a1871
shynome <shynome@gmail.com>
2024-05-18 20:26:37 +0800

    使用翻译文本

--- src/routes/[[lang]]/posts/[tag]/Toc.svelte
+++ src/routes/[[lang]]/posts/[tag]/Toc.svelte
@@ -29,6 +29,7 @@
 		table.style.marginRight = `${card.clientWidth / 2}px`
 	})
 	export let lv = 0
+	import { t } from '$lib/translations'
 </script>
 
 <ol class="ol p-0 m-1 pt-1 mt-0" bind:this={self} class:root={lv === 0} class:posts={isPosts}>
@@ -72,7 +73,7 @@
 </ol>
 {#if lv === 0 && isPosts}
 	<a class="d-block text-center border-top py-2" href={target} data-bs-toggle="collapse">
-		收起全文
+		{$t('common.collapse_artcile')}
 	</a>
 {/if}
 
